242574e9e33846d8d8b3971bd82fc01f46450398
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-16 17:33:02 -0500

    add #index and #show

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,12 @@
+require 'date'
+
 class Pet < ApplicationRecord
   SPECIES = %w[bird cat dog turtle snake]
   validates :name, presence: true
   validates :species, inclusion: { in: SPECIES }
+
+  def found_days_ago
+    days = (Date.today - found_on).to_i
+    "#{name} was found #{days} days ago"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'pets', to: 'pets#index'
+  get 'pets/:id', to: 'pets#show'
 end
